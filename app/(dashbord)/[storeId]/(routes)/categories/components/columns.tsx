0130c431810bb7571efe557fcfb7fcc4cc6e5828
AmrMustafa <90666301+AmrMustafa282@users.noreply.github.com>
2023-07-30 09:59:22 +0300

    first commit

---
+++ app/(dashbord)/[storeId]/(routes)/categories/components/columns.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { CellActino } from "./cell-action"
+
+
+export type CategoryColumn = {
+  id: string
+  name: string
+  billboardLabel:string
+  createdAt: string
+}
+
+export const columns: ColumnDef<CategoryColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell:({row}) =>row.original.billboardLabel,
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell:({ row })=><CellActino data={row.original} />
+  },
+]
---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import { ModalProvider } from '@/providers/modal-provider'
+
+import './globals.css'
+import { ToasterProvider } from '@/providers/toast-provider';
+import type { Metadata } from 'next'
+import prismadb from '@/lib/prismadb'
+import { ThemeProvider } from '@/providers/theme-provider';
+
+
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Admin Dashboard',
+  description: 'Admin Dashboard',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+  }) {
+  // const store = prismadb.store.find;
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem >
+            <ToasterProvider/>
+            <ModalProvider/>
+            {children}
+          </ThemeProvider>
+        </body>
+    </html>
+    </ClerkProvider>
+  )
+}
